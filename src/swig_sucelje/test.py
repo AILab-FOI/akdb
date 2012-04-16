7e150b5ef4532356a1d0768786eb00fa11e80cd1
krudjuras <krudjuras@995c5022-3e18-464e-9631-013bd5e168aa>
2012-04-16 14:32:58 +0000

    Testiranje rada struktura podataka

--- src/swig_sucelje/test.py
+++ src/swig_sucelje/test.py
@@ -6,3 +6,19 @@ kalashnikovDB.main()
 
 kalashnikovDB.AK_op_selection_test()
 kalashnikovDB.AK_op_projection_test()
+
+
+a = kalashnikovDB.list_elem
+a.type = 2
+a.size = 3
+a.data = "PODACI"
+a.table = "NAZIV TABLICE"
+a.attribute_name = "NAME"
+a.constraint = 123
+
+print a.type
+print a.size
+print a.data
+print a.table
+print a.attribute_name
+print a.constraint
