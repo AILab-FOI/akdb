1e8594811dddf25065cb09fb9972af0d88833265
kbilic <kbilic@995c5022-3e18-464e-9631-013bd5e168aa>
2012-06-04 12:48:17 +0000

    dopunjene My SQL / SQL Server / MS Access / Oracle sintakse

--- src/swig_sucelje/alter_table.py
+++ src/swig_sucelje/alter_table.py
@@ -3,14 +3,16 @@ from pyparsing import *
 
 #ALTER TABLE table_name ADD column_name data_type
 #ALTER TABLE table_name DROP COLUMN column_name
+#ALTER TABLE table_name ALTER COLUMN column_name data_type
+#ALTER TABLE table_name MODIFY column_name data_type
 
 LPAR, RPAR, COMMA = map(Suppress,"(),")
 alter_stmt = Forward().setName("alter table")
 
 #keywords
-(ALTER, TABLE, ADD, DROP, COLUMN ) =  map(CaselessKeyword, """ALTER, TABLE, ADD, DROP, COLUMN""".replace(",","").split())
+(ALTER, TABLE, ADD, DROP, MODIFY, COLUMN ) =  map(CaselessKeyword, """ALTER, TABLE, ADD, DROP, MODIFY, COLUMN""".replace(",","").split())
 
-keyword = MatchFirst((ALTER, TABLE, ADD, DROP, COLUMN )) 
+keyword = MatchFirst((ALTER, TABLE, ADD, DROP, MODIFY, COLUMN )) 
 
 identifier = ~keyword + Word(alphas, alphanums+"_")
 
@@ -18,17 +20,21 @@ table_name = identifier.copy()
 column_name = identifier.copy()
 data_type = identifier.copy()
 
-
-#single_source = ( Group(table_name("table") | table_name("table"))) 
-
 alter_core = (ALTER + TABLE + table_name)
 adding = (ADD + column_name + data_type) 
 dropping = (DROP + COLUMN + column_name)
+altering = (ALTER + COLUMN + column_name + data_type)
+modifying = (MODIFY + column_name + data_type)
 
-alter_stmt << (alter_core + Optional(adding | dropping))
+alter_stmt << (alter_core + Optional(adding | dropping | altering | modifying))
 
 tests = """\
-    alter table ime1 drop column stupac add stupac tip""".splitlines()
+    alter table ime1 drop column stupac add stupac tip
+    alter table ime2 add stupac tip
+    alter table ime3 alter column stupac tip
+    alter table ime4 modify stupac tip 
+    alter table ime5 drop column stupac alter column stupac tip""".splitlines()
+
 for t in tests:
     print t
     try:
