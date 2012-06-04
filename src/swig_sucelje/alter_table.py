72056fe1076bc58a5f5c92fee8f84327ffd048a1
kbilic <kbilic@995c5022-3e18-464e-9631-013bd5e168aa>
2012-06-04 12:15:54 +0000

    SQL naredba ALTER TABLE korištenjem PyParsinga

--- src/swig_sucelje/alter_table.py
+++ src/swig_sucelje/alter_table.py
@@ -5,7 +5,7 @@ from pyparsing import *
 #ALTER TABLE table_name DROP COLUMN column_name
 
 LPAR, RPAR, COMMA = map(Suppress,"(),")
-drop_stmt = Forward().setName("alter table")
+alter_stmt = Forward().setName("alter table")
 
 #keywords
 (ALTER, TABLE, ADD, DROP, COLUMN ) =  map(CaselessKeyword, """ALTER, TABLE, ADD, DROP, COLUMN""".replace(",","").split())
@@ -21,18 +21,18 @@ data_type = identifier.copy()
 
 #single_source = ( Group(table_name("table") | table_name("table"))) 
 
-drop_core = (ALTER + TABLE + table_name)
+alter_core = (ALTER + TABLE + table_name)
 adding = (ADD + column_name + data_type) 
 dropping = (DROP + COLUMN + column_name)
 
-drop_stmt << (drop_core + Optional(adding | dropping))
+alter_stmt << (alter_core + Optional(adding | dropping))
 
 tests = """\
-    alter table ime1 drop column stupac add stupac tip """.splitlines()
+    alter table ime1 drop column stupac add stupac tip""".splitlines()
 for t in tests:
     print t
     try:
-        print drop_stmt.parseString(t).dump()
+        print alter_stmt.parseString(t).dump()
     except ParseException, pe:
         print pe.msg
     print
