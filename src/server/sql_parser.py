1437fe830e3dd526386471b00138be8107354582
kivkovic <kivkovic@995c5022-3e18-464e-9631-013bd5e168aa>
2012-06-10 12:20:19 +0000

    Dodano parsiranje za delete, select i where, ispravljen insert

--- src/server/sql_parser.py
+++ src/server/sql_parser.py
@@ -1,10 +1,24 @@
 #@file sql_parser.py sql parser, currently only understands INSERT and CREATE TABLE
+#
+# autor: Matej Vidakovic
+# dopunio: Kresimir Ivkovic
+#
+# 2012-06-01 izmjene:
+# ispravljen insert into
+# dodan select
+# dodan delete
+# dodan where
 
 from pyparsing import *
 
 #literals
 createTableLit=CaselessKeyword("create table")
 insertLit=CaselessKeyword("insert into")
+deleteLit=CaselessKeyword("delete")
+selectLit=CaselessKeyword("select")
+fromLit=CaselessKeyword("from")
+usingLit=CaselessKeyword("using")
+whereLit=CaselessKeyword("where")
 lBracket=Suppress(Literal("("))
 rBracket=Suppress(Literal(")"))
 valuesLiteral=Suppress(CaselessLiteral("values"))
@@ -26,6 +40,7 @@ values=lBracket+valuesList+rBracket
 #columns
 columnName=identifier
 columnNameList=Group(delimitedList(columnName))
+columnNameListSelect=Group(delimitedList(columnName)) | '*'
 columns=lBracket+columnNameList+rBracket
 
 #data types
@@ -36,10 +51,13 @@ varcharType=CaselessKeyword("varchar")+dataSize
 
 #predicate(limited)
 binrelop=oneOf(["<", "<=", ">=", ">", "=", "!="])
+sqlIN=CaselessKeyword("IN")
+sqlBetween=CaselessKeyword("BETWEEN")
+sqlLike=CaselessKeyword("LIKE")
 sqlOR=CaselessKeyword("OR")
 sqlAND=CaselessKeyword("AND")
 predicate=Forward()
-predicate<<columnName+binrelop+value+ZeroOrMore((sqlOR | sqlAND)+predicate)
+predicate<<columnName+binrelop+value+ZeroOrMore((sqlOR | sqlAND | sqlIN | sqlBetween | sqlLike)+predicate)
 
 #constraints in atribute definition
 notNullConstraint=CaselessKeyword("NOT NULL")
@@ -68,8 +86,8 @@ atributes=atributeDefinitionList
 
 #INSERT INTO 
 insert=insertLit.setResultsName("commandName")+\
-        Optional(columns).setResultsName("columns")+\
         tableName.setResultsName("tableName")+\
+        Optional(columns).setResultsName("columns")+\
         valuesLiteral+\
         values.setResultsName("columnValues")+stringEnd
 
@@ -77,12 +95,49 @@ insert=insertLit.setResultsName("commandName")+\
 createTable=createTableLit.setResultsName("commandName")+\
              tableName.setResultsName("tableName")+\
              lBracket+\
-             atributes.setResultsName("atriburtes")+\
+             atributes.setResultsName("attributes")+\
              Optional(comma+endConstraints).setResultsName("endConstraints")+\
              rBracket+stringEnd
 
+#WHERE
+whereExpression=Forward()
+select=Forward()
+selectInner=Forward()
+
+
+#### za ugnijezdjene select upite unutar where-a treba staviti da select ide unutar liste (u uglate zagrade)
+whereCond = Group(
+				(columnName.setResultsName("whereCondLval") + binrelop.setResultsName("whereOp") + columnName.setResultsName("whereCondRval")) |
+				(columnName.setResultsName("whereCondLval") + binrelop.setResultsName("whereOp") + value.setResultsName("whereCondRval")) |
+				(columnName.setResultsName("whereCondLval") + sqlIN.setResultsName("whereOp") + values.setResultsName("whereCondRval")) |
+				(columnName.setResultsName("whereCondLval") + sqlIN.setResultsName("whereOp") + selectInner.setResultsName("whereCondRval"))
+			)
+
+where = whereLit.setResultsName("where")+\
+		whereExpression.setResultsName("whereExpression")
+
+whereExpression << whereCond + ZeroOrMore( ( sqlOR | sqlAND ) + whereExpression )
+
+            
+#SELECT
+selectStatement=selectLit.setResultsName("commandName")+\
+		columnNameListSelect.setResultsName("attributes")+\
+		fromLit.setResultsName("from")+\
+		tableName.setResultsName("tableName")+\
+		Optional(where.setResultsName("condition"))
+
+select << selectStatement + stringEnd
+selectInner << lBracket+selectStatement+rBracket
+
+#DELETE
+deleteFrom=deleteLit.setResultsName("commandName")+\
+		fromLit.setResultsName("from")+\
+		tableName.setResultsName("tableName")+\
+		Optional(usingLit.setResultsName("using")+tableName.setResultsName("usingTable"))+\
+		Optional(where.setResultsName("condition"))+stringEnd
+
 #add other commands with |
-sqlGrammar= insert | createTable
+sqlGrammar= insert | createTable | select | deleteFrom
 
 def AKparseSql(command):
     '''
@@ -98,4 +153,16 @@ def AKparseSql(command):
     except ParseException, err:
         return "Syntax error at char "+str(err.loc)+" "+err.msg
 
-
+if __name__ == '__main__':
+	print AKparseSql("insert into table (a, b, c) values(1, 2, 3)");
+	print AKparseSql("insert into table values(1, 2, 3)");
+	print AKparseSql("select * from tablica");
+	print AKparseSql("select a from tablica");
+	print AKparseSql("select a, b, c from tablica");
+	print AKparseSql("delete from tablica");
+	print AKparseSql("delete from tablica using b where a = b");
+	print AKparseSql("select a,b,c from a where b = c");
+	print AKparseSql("select * from b where b = c and c = 2 or x > 0");
+	print AKparseSql("select * from b where b in ('1','2')");
+	print AKparseSql("select * from b where b in (select * from c where c = 2)");
+	print AKparseSql("delete from b where a in (select c from d where c in (select e from e))");
