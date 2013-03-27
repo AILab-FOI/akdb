cda0f00efa563d5b7607a9bd63df5790f59da04f
ivpusic <ivpusic@995c5022-3e18-464e-9631-013bd5e168aa>
2013-03-27 00:33:26 +0000

    I started to implement observable pattern

--- src/observable.c
+++ src/observable.c
@@ -1,5 +1,5 @@
 /**
-@file observable.c File that provides implementations of functions
+@file observable.c File that provides implementations of functions for observable pattern
  */
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -19,7 +19,98 @@
 
 #include "observable.h"
 
+/** 
+ * @author Ivan Pusic
+ * @brief Functions to run some action on receiving message from some observer
+ * 
+ * @return Exit status
+ */
+static inline int *AK_run_action()
+{
+    printf("Hello from observable pattern :]");
+    return OK;
+}
+
+/** 
+ * @author Ivan Pusic
+ * @brief Function to registering new observer. Observer is added to list of all observers
+ * @param self Observable object instance
+ * @param observer Observer object instance
+ * 
+ * @return Exit status 
+ */
+static inline int *AK_register_observer(AK_observable *self, AK_observer *observer)
+{
+    return OK;
+}
+
+/** 
+ * @author Ivan Pusic
+ * @brief Function to unregister observer. Observer will be removed from list of all observers
+ * @param self Observable object instance
+ * @param observer Observer object instance
+ * 
+ * @return Exit status
+ */
+static inline int *AK_unregister_observer(AK_observable *self, AK_observer *observer)
+{
+    return OK;
+}
+
+/** 
+ * @author Ivan Pusic
+ * @brief Function to notiy certain observer
+ * @param self Observable object instance
+ * @param observer Observer object to notify
+ * 
+ * @return Exit status
+ */
+static inline int *AK_notify_observer(AK_observable *self, AK_observer *observer)
+{
+    return OK;
+}
+
+/** 
+ * @author Ivan Pusic
+ * @brief Function to notify all observers
+ * @param self Observable object instance
+ * 
+ * @return Exit status
+ */
+static inline int *AK_notify_observers(AK_observable self)
+{
+    return OK;
+}
+
+/** 
+ * @author Ivan Pusic
+ * @brief Function for initializing observable object
+ *
+ * @return Pointer to new observable object
+ */
+AK_observable *AK_init_observable()
+{
+    AK_observable *self;
+    self = (AK_observable*) calloc(1, sizeof(self));
+    self->AK_register_observer = AK_register_observer;
+    self->AK_unregister_observer = AK_unregister_observer;
+    self->AK_run_action = AK_run_action;
+    self->AK_notify_observer = AK_notify_observer;
+    self->AK_notify_observers = AK_notify_observers;
+
+    return self;
+}
+
+/**
+ * @author Ivan Pusic
+ * @brief Function that runs tests for observable pattern
+ */
 void AK_observable_test()
 {
-    printf ("\n========== OBSERVABLE PATTERN TEST ==========\n");
+    printf ("\n========== OBSERVABLE PATTERN BEGIN ==========\n");
+    
+    AK_observable *observableObject = AK_init_observable();
+    observableObject->AK_run_action();
+    
+    printf ("\n========== OBSERVABLE PATTERN END ==========\n");
 }
--- src/observable.h
+++ src/observable.h
@@ -1,6 +1,6 @@
 /**
-@file observable.h Header file that provides data structures and declarations of functions for observable design pattern
- */
+   @file observable.h Header file that provides data structures and declarations of functions for observable pattern
+*/
 /*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,4 +17,45 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
  */
 
-void AK_observable_test();
+#ifndef OBSERVABLE
+#define OBSERVABLE
+#define MAX_OBSERVABLE_SERVERS 10
+
+#include "constants.h"
+
+/**
+ * @author Ivan Pusic
+ * @struct Observer
+ * @brief Structure defines functions for observer object
+ */
+struct Observer {
+    void (*AK_destroy_observer) (struct Observer*);
+    void (*AK_notify_handler) (struct Observer*, void*);
+};
+typedef struct Observer AK_observer;
+
+/**
+ * @author Ivan Pusic
+ * @struct Observable
+ * @brief Structure defines functions for observable object
+ */
+struct Observable {
+    // Members
+    AK_observer observers[MAX_OBSERVABLE_SERVERS];
+
+    // Methods
+    int (*AK_destroy_observable) (struct Observable*);
+    int (*AK_register_observer) (struct Observable*, struct AK_observer*);
+    int (*AK_unregister_observer) (struct Observable*, struct AK_observer*);
+    int (*AK_run_action) ();
+    int (*AK_notify_observer) (struct Observable*, struct AK_observer*);
+    int (*AK_notify_observers) (struct Observable*);
+};
+typedef struct Observable AK_observable;
+
+#endif
+
+extern AK_observer *AK_init_observer();
+extern AK_observable *AK_init_observable();
+
+extern void AK_observable_test();
