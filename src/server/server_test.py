c18058d2591c6095d7807cb380314b93731f48d5
dvukelic0 <dvukelic0@995c5022-3e18-464e-9631-013bd5e168aa>
2013-04-17 10:20:27 +0000

    reorganizacija koda

--- src/server/server_test.py
+++
@@ -1,8 +0,0 @@
-#file server_test.py runs the server for testing
-
-from server import *
-
-ip="localhost"
-port=1999
-server=AK_server((ip, port), AK_server_request_handler)
-server.serve_forever()
