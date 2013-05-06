f19348c4a940c3fce908a2df918ec10533c8d328
dasacer1 <dasacer1@995c5022-3e18-464e-9631-013bd5e168aa>
2013-05-06 08:56:53 +0000

    implemented handling with view query and view rel_exp

--- src/sql/view.c
+++ src/sql/view.c
@@ -38,19 +38,64 @@ int AK_get_view_obj_id(char *name) {
 }
 
 /**
+ * @author Danko Sačer
+ * @brief Returnes a query by its name
+ * @return query string or error
+ */
+char* AK_get_view_query(char *name){
+   int i = 0;
+   char *query;
+    AK_list *row;
+    while ((row = (AK_list*)AK_get_row(i++, "AK_view"))) {
+        if (!strcmp(row->next->next->data, name)) {
+            query = row->next->next->next->data;
+	    
+	    return query;
+        }
+    }
+    
+    return EXIT_ERROR;
+}
+
+/**
+ * @author Danko Sačer
+ * @brief Returnes a relation expression by its name
+ * @return rel_exp string or error
+ */
+char* AK_get_rel_exp(char *name){
+   int i = 0;
+   char *rel_exp;
+    AK_list *row;
+    while ((row = (AK_list*)AK_get_row(i++, "AK_view"))) {
+        if (!strcmp(row->next->next->data, name)) {
+            rel_exp = row->next->next->next->next->data;
+	    
+	    return rel_exp;
+        }
+    }
+    
+    return EXIT_ERROR;
+}
+
+/**
  * @author Kresimir Ivkovic
  * @brief Adds a new view to the view table with the corresponding name and value (view query); set_id is optional, if it's not set, the system will determine the new id automatically
  * @return Id of the newly inserted view
  */
-int AK_view_add(char *name, char *query, int set_id){
+int AK_view_add(char *name, char *query, char *rel_exp, int set_id){
     char *tblName = "AK_view";
     AK_list_elem row_root = (AK_list_elem) malloc(sizeof (AK_list));
     Ak_Init_L(row_root);
     int view_id = AK_get_id();
     if(set_id!=0) view_id = set_id;
     Ak_Insert_New_Element(TYPE_INT, &view_id, tblName, "obj_id", row_root);
+    //adding prefics '_view' to view name for integration of view relation expression (used in query_optimization.c for now) by Danko Sacer NOT SET
+    //char set_name[strlen(name)];
+    //strcpy(set_name, name);
+    //strcat(set_name, "_view");
     Ak_Insert_New_Element(TYPE_VARCHAR, name, tblName, "name", row_root);
     Ak_Insert_New_Element(TYPE_VARCHAR, query, tblName, "query", row_root);
+    Ak_Insert_New_Element(TYPE_VARCHAR, rel_exp, tblName, "rel_exp", row_root);
     Ak_insert_row(row_root);
     return view_id;
 }
@@ -92,16 +137,19 @@ int AK_view_rename(char *name, char *new_name){
    int result = 0, i = 0;
    int view_id;
    char *query;
+   char *rel_exp;
    AK_list *row;
    while ((row = (AK_list*)AK_get_row(i++, "AK_view"))) {
         if (!strcmp(row->next->next->data, name)) {
             memcpy(&view_id, row->next->data, sizeof(int));
             query = row->next->next->next->data;
+	    rel_exp = row->next->next->next->next->data;
         }
     }
+
    result = AK_view_remove_by_name(name);
    result = AK_view_remove_by_name(name);
-   result = AK_view_add(new_name, query, view_id);
+   result = AK_view_add(new_name, query, rel_exp, view_id);
    return result;
 }
 
@@ -110,10 +158,11 @@ int AK_view_rename(char *name, char *new_name){
  * @brief Changes the query for a view (determined by it's name) to "query"
  * @return error or success 
  */
-int AK_view_change_query(char *name, char *query){
+int AK_view_change_query(char *name, char *query, char *rel_exp){
    int view_id = AK_get_view_obj_id(name);
+   //char *rel_exp = AK_get_rel_exp(name);
    int result = AK_view_remove_by_name(name);
-   result = AK_view_add(name, query, view_id);
+   result = AK_view_add(name, query, rel_exp, view_id);
    return result;
 }
 
@@ -123,30 +172,42 @@ int AK_view_change_query(char *name, char *query){
  * @return 
  */
 void AK_view_test() {
-   AK_view_add("view1", "query1", 0);
-   AK_view_add("view2", "query2", 0);
-   AK_view_add("view3", "query3", 0);
-   AK_view_add("view4", "query4", 0);
-   AK_view_add("view5", "query5", 0);
+   printf("\n*******View test**********\n\n");
+   AK_view_add("view1", "SELECT lastname FROM profesor","profesor;lastname;", 0);
+   AK_view_add("view2", "query2","firstname;", 0);
+   AK_view_add("view3", "query3","*;profesor;", 0);
+   AK_view_add("view4", "query4","2 >;", 0);
+   AK_view_add("view5", "query5","id_object;", 0);
    AK_print_table("AK_view");
 
    printf("Obj_id za view1: %d\n", AK_get_view_obj_id("view1"));
+   //added query strings and rel_exp string result by Danko Sacer
+   printf("Query za view1: %s\n\n", AK_get_view_query("view1"));
+   printf("Rel_exp za view1: %s\n\n", AK_get_rel_exp("view1"));
+
    printf("Obj_id za view2: %d\n", AK_get_view_obj_id("view2"));
+   printf("Query za view2: %s\n\n", AK_get_view_query("view2"));
+
    printf("Obj_id za view3: %d\n", AK_get_view_obj_id("view3"));
+   printf("Query za view3: %s\n\n", AK_get_view_query("view3"));
+
    printf("Obj_id za view4: %d\n", AK_get_view_obj_id("view4"));
+   printf("Query za view4: %s\n\n", AK_get_view_query("view4"));
+
    printf("Obj_id za view5: %d\n", AK_get_view_obj_id("view5"));
+   printf("Query za view5: %s\n\n", AK_get_view_query("view5"));
 
    printf("\nRemoving view 'view1' and 'view2'...\n");
    AK_view_remove_by_name("view1");
    AK_view_remove_by_name("view2");
    AK_print_table("AK_view");
 
-   printf("\nRenaming view 'view3' to 'view300'...\n");
+   printf("\nRenaming 'view3' to 'view300'...\n");
    AK_view_rename("view3","view300");
    AK_print_table("AK_view");
 
    printf("\nChanging 'view4' query to '44query44'...\n");
-   AK_view_change_query("view4","44query44");
+   AK_view_change_query("view4","44query44","student;id");
    AK_print_table("AK_view");
   
 }
