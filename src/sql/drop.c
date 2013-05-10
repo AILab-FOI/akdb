121b4f91d27d5e5ce371d6c3fcde2a9d28cd012f
juhlevnja1 <juhlevnja1@995c5022-3e18-464e-9631-013bd5e168aa>
2013-05-10 08:07:04 +0000

    Bug fixed and make drop function working, reorganized code

--- src/sql/drop.c
+++ src/sql/drop.c
@@ -23,177 +23,174 @@
 
 #define DROP_TABLE 0
 
+void AK_drop(int type, AK_drop_arguments *drop_arguments) {
 
-void AK_drop(int type, AK_drop_arguments* drop_arguments) 
-{
-  AK_print_table("AK_relation");
-  AK_list_elem row_root;
-  
-  char* tblName;
-  
-  switch (type) 
-  {
-    case DROP_TABLE:
-      
-     
-     tblName = (char*)drop_arguments->value;
+    char *tblName;
+
+    switch (type) {
+        case DROP_TABLE:
+
+            tblName = (char*) drop_arguments->value;
+
+            if (!AK_table_exist(tblName) == 0) {
+
+                table_addresses *addresses;
+
+                //addresses = AK_get_segment_addresses(tblName, SEGMENT_TYPE_TABLE);
+                addresses = (table_addresses*) AK_get_table_addresses(tblName);
+
+                AK_mem_block *mem_block;
+
+                int from = 0, to = 0, i = 0, j = 0, c;
+
+                for (j = 0; j < MAX_EXTENTS_IN_SEGMENT; j++) {
+                    if (addresses->address_from != 0) {
+                        from = addresses->address_from[j];
+                        to = addresses->address_to[j];
+
+                        if (from == 0 || to == 0) break;
+                        for (i = from; i <= to; i++) {
+                            mem_block = (AK_mem_block *) AK_get_block(i);
+                            mem_block->block->type = BLOCK_TYPE_FREE;
+
+                            for (c = 0; c < DATA_BLOCK_SIZE; c++) {
+                                mem_block->block->tuple_dict[c].type = FREE_INT;
+                                mem_block->block->data[c] = FREE_CHAR;
+                            }
+                        }
+                    } else break;
+                }
 
 
-    table_addresses *addresses2;
-    addresses2 = AK_get_segment_addresses(tblName, SEGMENT_TYPE_TABLE);
+                //int segmentType = SEGMENT_TYPE_TABLE;
+                int data_adr = 0;
+                int data_size = 0;
+                int data_type = 0;
+                int address_sys;
+                char name_sys[MAX_ATT_NAME];
+                char *sys_table;
+                sys_table = "AK_relation";
 
-    AK_mem_block *mem_block2;
-    int from = 0, to = 0, j1 = 0, i1 = 0;
-    int c;
+                /*
+                switch (segmentType) {
+                case SEGMENT_TYPE_TABLE:
+                sys_table = "AK_relation";
+                break;
+                case SEGMENT_TYPE_INDEX:
+                sys_table = "AK_index";
+                break;
+                }
+                 */
 
-    for (j1 = 0; j1 < MAX_EXTENTS_IN_SEGMENT; j1++) {
-        if (addresses2->address_from != 0) {
-            from = addresses2->address_from[j1];
-            to = addresses2->address_to[j1];
-	    
-	    if (from == 0 || to == 0) break;
-            for (i1 = from; i1 <= to; i1++) {
-                mem_block2 = (AK_mem_block *) AK_get_block(i1);
-		mem_block2->block->type = BLOCK_TYPE_FREE;
-		
-		for (c = 0; c < DATA_BLOCK_SIZE; c++) mem_block2->block->tuple_dict[c].type = FREE_INT;
-		
-								mem_block2->block->data[0] = FREE_CHAR;
+                AK_mem_block *mem_block2 = (AK_mem_block *) AK_get_block(0);
+
+                for (i = 0; i < DATA_BLOCK_SIZE; i++) {
+                    memset(name_sys, 0, MAX_ATT_NAME);
+
+                    if (mem_block2->block->tuple_dict[i].address == FREE_INT) {
+                        break;
+                    }
+
+                    data_adr = mem_block2->block->tuple_dict[i].address;
+                    data_size = mem_block2->block->tuple_dict[i].size;
+                    data_type = mem_block2->block->tuple_dict[i].type;
+                    memcpy(name_sys, mem_block2->block->data + data_adr, data_size);
+
+                    i++;
+                    data_adr = mem_block2->block->tuple_dict[i].address;
+                    data_size = mem_block2->block->tuple_dict[i].size;
+                    data_type = mem_block2->block->tuple_dict[i].type;
+                    memcpy(&address_sys, mem_block2->block->data + data_adr, data_size);
+
+                    if (strcmp(name_sys, sys_table) == 0) {
+                        break;
+                    }
+                }
+
+                mem_block2 = (AK_mem_block *) AK_get_block(address_sys);
+                table_addresses *addresses2;
+
+                //addresses2 = (table_addresses *) malloc(sizeof (table_addresses));
+                addresses2 = (table_addresses*) AK_get_table_addresses(tblName);
+
+                //memset(addresses2->address_from, 0, MAX_EXTENTS_IN_SEGMENT);
+                //memset(addresses2->address_to, 0, MAX_EXTENTS_IN_SEGMENT);
+
+                for (i = 0; i < MAX_EXTENTS_IN_SEGMENT; i++) {
+                    addresses2->address_from[i] = 0;
+                    addresses2->address_to[i] = 0;
+                }
+
+
+                char name[MAX_VARCHAR_LENGTH];
+                //int address_from;
+                //int address_to;
+
+                for (i = 0; i < DATA_BLOCK_SIZE; i++) {
+                    if (mem_block2->block->tuple_dict[i].type == FREE_INT)
+                        break;
+                    i++;
+                    memcpy(name, &(mem_block2->block->data[mem_block2->block->tuple_dict[i].address]), mem_block2->block->tuple_dict[i].size);
+                    name[ mem_block2->block->tuple_dict[i].size] = '\0';
+                    if (strcmp(name, tblName) == 0) {
+                        i++;
+                        mem_block2->block->data[mem_block2->block->tuple_dict[i].address] = 0;
+                        i++;
+                        mem_block2->block->data[mem_block2->block->tuple_dict[i].address] = 0;
+                    }
+
+                    /*   i++;
+                         memcpy(&address_from, &(mem_block2->block->data[mem_block2->block->tuple_dict[i].address]), mem_block2->block->tuple_dict[i].size);
+                         i++;
+                         memcpy(&address_to, &(mem_block2->block->data[mem_block2->block->tuple_dict[i].address]), mem_block2->block->tuple_dict[i].size);
+
+                         //if found the table that addresses we need
+                         if (strcmp(name, tblName) == 0) {
+                             addresses2->address_from[j] = address_from;
+                             addresses2->address_to[j] = address_to;
+                             j++;
+                         }
+                         if (segmentType == SEGMENT_TYPE_INDEX) {
+                             i += 2;
+                         }
+                     */
+
+                }
+
+
+                AK_list_elem row_root = (AK_list_elem) malloc(sizeof (AK_list_elem));
+                Ak_Init_L(row_root);
+                Ak_DeleteAll_L(row_root);
+
+                Ak_Insert_New_Element_For_Update(TYPE_VARCHAR, tblName, "AK_relation", "name", row_root, 1);
+
+                Ak_delete_row(row_root);
+
+                
+                printf("TABLE %s DROPPED!\n", tblName);
+
             }
-        } else break;
-    }
-    
-    
-    
-    
 
-
-
-
-   int i = 0;
-    int freeVar = 0;
-    int data_adr = 0;
-    int data_size = 0;
-    //int data_type = 0;
-    int address_sys;
-    char name_sys[MAX_ATT_NAME];
-    char *sys_table;
-
-  //  switch (segmentType) {
-    //    case SEGMENT_TYPE_TABLE:
-            sys_table = "AK_relation";
-      //      break;
-      //  case SEGMENT_TYPE_INDEX:
-       //     sys_table = "AK_index";
-         //   break;
-    //}
-
-	//Ak_dbg_messg(HIGH, MEMO_MAN,"get_segment_addresses: Serching for %s table \n", sys_table);
-    AK_mem_block *mem_block = (AK_mem_block *) AK_get_block(0);
-
-    for (i = 0; i < DATA_BLOCK_SIZE; i++) {
-        memset(name_sys, 0, MAX_ATT_NAME);
-
-        if (mem_block->block->tuple_dict[i].address == FREE_INT) {
             break;
-        }
 
-        data_adr = mem_block->block->tuple_dict[i].address;
-        data_size = mem_block->block->tuple_dict[i].size;
-        //data_type = mem_block->block->tuple_dict[i].type;
-        memcpy(name_sys, mem_block->block->data + data_adr, data_size);
 
-        i++;
-        data_adr = mem_block->block->tuple_dict[i].address;
-        data_size = mem_block->block->tuple_dict[i].size;
-        //data_type = mem_block->block->tuple_dict[i].type;
-        memcpy(&address_sys, mem_block->block->data + data_adr, data_size);
-
-        if (strcmp(name_sys, sys_table) == 0) {
-	//		Ak_dbg_messg(HIGH, MEMO_MAN, "get_segment_addresses: Found the address of the %s table: %d \n", sys_table, address_sys);
+        default:
             break;
-        }
     }
-
-    mem_block = (AK_mem_block *) AK_get_block(address_sys);
-    table_addresses * addresses = (table_addresses *) malloc(sizeof (table_addresses));
-
-    //memset(addresses->address_from, 0, MAX_EXTENTS_IN_SEGMENT);
-    //memset(addresses->address_to, 0, MAX_EXTENTS_IN_SEGMENT);
-
-    for (freeVar = 0; freeVar < MAX_EXTENTS_IN_SEGMENT; freeVar++) {
-        addresses->address_from[freeVar] = 0;
-        addresses->address_to[freeVar] = 0;
-    }
-
-    char name[MAX_VARCHAR_LENGTH];
-
-    for (i = 0; i < DATA_BLOCK_SIZE; i++) {
-        if (mem_block->block->tuple_dict[i].type == FREE_INT)
-            break;
-        i++;
-        memcpy(name, &(mem_block->block->data[mem_block->block->tuple_dict[i].address]), mem_block->block->tuple_dict[i].size);
-        name[ mem_block->block->tuple_dict[i].size] = '\0';
-        if (strcmp(name, tblName) == 0) {
-	  //  mem_block->block->data[mem_block->block->tuple_dict[i].address] = 0;
-	    i++;
-	    mem_block->block->data[mem_block->block->tuple_dict[i].address] = 0;
-	    i++;
-	    mem_block->block->data[mem_block->block->tuple_dict[i].address] = 0;
-	}
-   /*     i++;
-        memcpy(&address_from, &(mem_block->block->data[mem_block->block->tuple_dict[i].address]), mem_block->block->tuple_dict[i].size);
-        i++;
-        memcpy(&address_to, &(mem_block->block->data[mem_block->block->tuple_dict[i].address]), mem_block->block->tuple_dict[i].size);
-
-        //if found the table that addresses we need
-        if (strcmp(name, tblName) == 0) {
-            addresses->address_from[j] = address_from;
-            addresses->address_to[j] = address_to;
-            j++;
-        }
-        if (segmentType == SEGMENT_TYPE_INDEX) {
-            i += 2;
-        }*/
-    }
- //   return addresses;
-    
-    
-    
-    
-    
-    
-          //tblName = "AK_relation";
-   
-       row_root = (AK_list_elem) malloc(sizeof (AK_list));
-      Ak_Init_L(row_root);
-      Ak_DeleteAll_L(row_root);
-   
-   //char *name2 = (char*) drop_arguments->next->value;
-      Ak_Insert_New_Element_For_Update(TYPE_VARCHAR, tblName, "AK_relation", "name", row_root, 1);
-  // Ak_Insert_New_Element_For_Update(TYPE_VARCHAR, "FOI", tblName, "firstname", row_root, 0);
-
-    Ak_delete_row(row_root);
-    
-    
-    
-   //AK_flush_cache();
-    
-    AK_print_table("AK_relation");
-    AK_print_table(tblName);
-    
-      
-    break;
-    default:
-      break;
-  }
 }
 
-
-
-void AK_drop_test()
-{
-  AK_drop_arguments* drop_arguments = malloc(sizeof (AK_drop_arguments));
-  drop_arguments->value = "employee";
-  AK_drop(DROP_TABLE, drop_arguments);
+void AK_drop_test() {
+    printf("=============================================\n");
+    printf("==================DROP_TEST==================\n");
+    AK_print_table("AK_relation");
+    AK_drop_arguments *drop_arguments = malloc(sizeof (AK_drop_arguments));
+    drop_arguments->value = "department";
+    AK_drop(DROP_TABLE, drop_arguments);
+    AK_print_table("department");
+    AK_print_table("AK_relation");
+    drop_arguments->value = "professor2";
+    AK_drop(DROP_TABLE, drop_arguments);
+    AK_print_table("professor2");
+    AK_print_table("AK_relation");
+    printf("================END_DROP_TEST================\n");
 }
