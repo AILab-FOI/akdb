3db228889ff3f3e8afdbfa4a042e31c2330e8928
renatam <renatam@995c5022-3e18-464e-9631-013bd5e168aa>
2013-05-16 19:54:14 +0000

    Select relational operator header.

---
+++ src/sql/select.h
@@ -0,0 +1,6 @@
+#include "../file/table.h"
+#include "../file/fileio.h"
+#include "../rel/selection.h"
+
+int AK_select(char *srcTable,char *destTable,AK_list *atributi,AK_list *uvjet);
+void AK_select_test();
