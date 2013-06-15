ff5e4b8cd11a67924f26ef6dff2f1a5f172bcf0b
dasacer1 <dasacer1@995c5022-3e18-464e-9631-013bd5e168aa>
2013-06-15 14:15:08 +0000

    implemented test for executor of create sequence

--- src/srv/sql_executor_test.py
+++ src/srv/sql_executor_test.py
@@ -1,8 +1,8 @@
-import sys
-sys.path.append('../swig/')
-import kalashnikovDB as ak47
+#import sys
+#sys.path.append('../swig/')
+#import kalashnikovDB as ak47
 from sql_executor import *
-
+'''
 ak47.AK_inflate_config()
 ak47.AK_init_disk_manager()
 ak47.AK_memoman_init()
@@ -12,7 +12,6 @@ bulbasaur = sql_executor()
 student_attr_name = ["id_student", "firstname", "lastname", "year", "weight"]
 student_attr_type = [ak47.TYPE_INT, ak47.TYPE_VARCHAR, ak47.TYPE_VARCHAR, ak47.TYPE_INT, ak47.TYPE_FLOAT]
 student_attr_values = ['2','Pero','Peric','1991','100.2']
-
 
 ak47.create_header_test("student", student_attr_name, student_attr_type)
 
@@ -24,3 +23,25 @@ print "2 - " + str(bulbasaur.insert("INSERT INTO student(id_student, firstnames,
 print "\n------------------------------------ PRINT TABLE STUDENT ------------------------------------\n"
 
 ak47.AK_print_table("student")
+'''
+#------------test executor for create sequence by Danko Sacer----------------
+print "\n------------Testing executor for create sequence by Danko Sacer----------------\n"
+
+print "\nCreate sequence brojac_1 start with 1 increment by 2 minvalue 0 maxvalue 100 cycle 1"
+
+initialize()
+test = sql_sequence()
+
+if(test.create_sequence("Create sequence brojac_1 start with 1 increment by 2 minvalue 0 maxvalue 100 cycle 1")==True):
+	print "Everything was fine\n"
+else:
+	print "Something's gone wrong. Unable to create sequence!\n"
+
+ak47.AK_print_table("AK_sequence")
+
+# next query has overflow buffer error
+
+#print "\nSljedeci upit\n"
+#test.create_sequence("Create sequence brojac_2 start with 1 increment by 2 minvalue 0 maxvalue 100")
+#ak47.AK_print_table("AK_sequence")
+
