c823ec96c2aaae91ec09302ef39d5d804ff19483
nmakar <nmakar@foi.hr>
2015-05-05 20:21:11 +0200

    Update id.c
    Small code cleanup

--- src/file/id.c
+++ src/file/id.c
@@ -32,9 +32,9 @@ int AK_get_id() {
     Ak_Init_L3(&row_root); 
 	
 	/*Assumption was that objectID is always in the first row of table AK_sequence. If in future, for some reason, that won't be the case
-	 * then check all rows of table AK_sequence (for()) and update a row which contains objectID (Ak_GetNth_L2(2, row), value in column 
-	 * name must be objectID)*/
-	 
+	 * then check all rows of table AK_sequence (for(i=0; i<num_rec; i++)) and update a row which contains objectID (Ak_GetNth_L2(2, row), value in column 
+	 * name must be objectID) or create a row which will contain objectID*/
+	
 	int num_rec = AK_get_num_records("AK_sequence");
 	
     if(num_rec > 0) {
