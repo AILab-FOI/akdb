07ae6ff60fb9863994df8ce3bdb7a7deff96aa5d
Hrvoje <hrvoje.orejas@gmail.com>
2016-06-14 11:21:05 +0200

    fixed sql executor

--- src/srv/sql_executor.py
+++ src/srv/sql_executor.py
@@ -148,18 +148,17 @@ class Print_table_command:
         print_regex = r"^\\p\s+([a-zA-Z0-9_]+)\s*$"
         pattern = None
         matcher = None
-
         ## matches method
         # checks whether given input matches print command syntax
         def matches(self, input):
                 self.pattern = re.compile(self.print_regex)
                 self.matcher = self.pattern.match(input)
-                return self.matcher != None
+                return self.matcher if self.matcher is not None else None
 
         ## execute method
         # defines what is called when print command is invoked
         def execute(self):
-                ak47.AK_print_table(self.matcher.group(1))
+                ak47.AK_print_table(self.matcher.group(0))
                 return "OK"
 
 ## table_derails_command
@@ -175,7 +174,7 @@ class Table_details_command:
         def matches(self, input):
                 self.pattern = re.compile(self.table_details_regex)
                 self.matcher = self.pattern.match(input)
-                return self.matcher != None
+                return self.matcher if self.matcher is not None else None
 
         ## execute method
         # defines what is called when table_details command is invoked
@@ -196,7 +195,7 @@ class Table_exists_command:
         def matches(self, input):
                 self.pattern = re.compile(self.table_details_regex)
                 self.matcher = self.pattern.match(input)
-                return self.matcher != None
+                return self.matcher if self.matcher is not None else None
 
         ## execute method
         # defines what is called when table_exists command is invoked
@@ -338,7 +337,7 @@ class Create_index_command:
         ## matches method
         # checks whether given input matches table_exists command syntax
         def matches(self, input):
-                self.pattern = re.compile(self.create_index_regex)
+                self.pattern = re.compile(self.create_table_regex)
                 self.matcher = self.pattern.match(input)
                 self.expr = input
                 if (self.matcher is not None):
@@ -455,7 +454,8 @@ class Insert_into_command:
     def matches(self,inp):
         self.pattern = re.compile(self.insert_into_regex)
         self.matcher = self.pattern.match(inp)
-        return self.matcher != None
+        return self.matcher if self.matcher is not None else None
+
 
 
     def execute(self):
@@ -592,7 +592,8 @@ class Grant_command:
 		self.pattern = re.compile(self.table_details_regex, re.IGNORECASE)
 		self.matcher = self.pattern.match(input)
 		self.command = input
-		return self.matcher != None
+		return self.matcher if self.matcher is not None else None
+
 
 	def execute(self):
 		print "Printing out: "
@@ -1007,95 +1008,95 @@ class sql_executor:
         # checks whether received command matches any of the defined commands for kalashnikovdb, 
         # and call its execution if it matches
         def commands_for_input(self, command):
-                for elem in self.commands:
-                        if (elem.matches(command) is not None):
-                                return elem.execute()
-                return "Wrong command."
+            for elem in self.commands:
+                if (elem.matches(command) is not None):
+                    return elem.execute()
+            return "Wrong command."
 
         ## execute method
         # called when a new command is received (from client)
         def execute(self, command):
-                return self.commands_for_input(command)
+            return self.commands_for_input(command)
 
         ## insert
         # executes the insert expression
         # @param self object pointer
         # @parama expr the expression to be executed 
         def insert(self,expr):
-                parser = sql_tokenizer()
-                token = parser.AK_parse_insert_into(expr)
-                if isinstance(token, str):
-                        print "Error: syntax error in expression"
-                        print expr
-                        print token
-                        return False
-                table_name = str(token.tableName)
-                # is there a table
-                if (ak47.AK_table_exist(table_name) == 0):
-                        print "Error: table '"+ table_name +"' does not exist"
-                        return False
-                # data values for insert
-                insert_attr_values = map(lambda x: x.replace("'",""),list(token.columnValues[0]))
-                # data types for insert
-                insert_attr_types = map(lambda x: get_attr_type(x.replace("'","")),list(token.columnValues[0]))
-                # get array of attributes for table
-                table_attr_names = str(ak47.AK_rel_eq_get_atrributes_char(table_name)).split(";")
-                # get attribute types for table
-                table_attr_types = str(ak47.AK_get_table_atribute_types(table_name)).split(";")
-                # attribute names for insert
-                insert_attr_names = table_attr_names
-                # attributes for insert
-                if(token.columns):
-                        insert_attr_names = []
-                        table_types_temp = table_attr_types
-                        table_attr_types = []
-                        insert_columns = list(token.columns[0])
-                        for index,col in enumerate(insert_columns):
-                                if col not in table_attr_names:
-                                        print "\nError: table has no attribute '" + str(col) + "':"
-                                        akdbError(expr,col)
-                                        return False
-                        # check attributes for insert
-                        for ic,col in enumerate(insert_columns):
-                                for ia,tab in enumerate(table_attr_names):
-                                        if col == tab:
-                                                if tab not in insert_attr_names:
-                                                        insert_attr_names.append(tab)
-                                                        table_attr_types.append(int(table_types_temp[ia]))
-                                                else:
-                                                        print "\nError: duplicate attribute " + tab + ":"
-                                                        akdbError(expr,tab)
-                                                        return False
+            parser = sql_tokenizer()
+            token = parser.AK_parse_insert_into (expr)
+            if isinstance(token, str):
+                    print "Error: syntax error in expression"
+                    print expr
+                    print token
+                    return False
+            table_name = str(token.tableName)
+            # is there a table
+            if (ak47.AK_table_exist(table_name) == 0):
+                    print "Error: table '"+ table_name +"' does not exist"
+                    return False
+            # data values for insert
+            insert_attr_values = map(lambda x: x.replace("'",""),list(token.columnValues[0]))
+            # data types for insert
+            insert_attr_types = map(lambda x: get_attr_type(x.replace("'","")),list(token.columnValues[0]))
+            # get array of attributes for table
+            table_attr_names = str(ak47.AK_rel_eq_get_atrributes_char(table_name)).split(";")
+            # get attribute types for table
+            table_attr_types = str(ak47.AK_get_table_atribute_types(table_name)).split(";")
+            # attribute names for insert
+            insert_attr_names = table_attr_names
+            # attributes for insert
+            if(token.columns):
+                    insert_attr_names = []
+                    table_types_temp = table_attr_types
+                    table_attr_types = []
+                    insert_columns = list(token.columns[0])
+                    for index,col in enumerate(insert_columns):
+                            if col not in table_attr_names:
+                                    print "\nError: table has no attribute '" + str(col) + "':"
+                                    akdbError(expr,col)
+                                    return False
+                    # check attributes for insert
+                    for ic,col in enumerate(insert_columns):
+                            for ia,tab in enumerate(table_attr_names):
+                                    if col == tab:
+                                            if tab not in insert_attr_names:
+                                                    insert_attr_names.append(tab)
+                                                    table_attr_types.append(int(table_types_temp[ia]))
+                                            else:
+                                                    print "\nError: duplicate attribute " + tab + ":"
+                                                    akdbError(expr,tab)
+                                                    return False
 
-                        if (len(insert_columns) == len(insert_attr_values)):
-                                for index,tip in enumerate(insert_attr_types):
-                                        if int(insert_attr_types[index]) != int(table_attr_types[index]):
-                                                type_name = get_type_name(int(table_attr_types[index]))
-                                                print "\nError: type error for attribute '" + insert_attr_names[index] + "':"
-                                                akdbError(expr,insert_attr_values[index])
-                                                print "Expected: " + type_name
-                                                return False
-                        else:
-                                print "\nError: attribute names number not matching attribute values number supplied for table '" + table_name + "':"
-                                akdbError(expr,insert_columns[0])
-                                return False
-                # values for insert
-                elif (len(table_attr_names) < len(insert_attr_values)):
-                        print "\nError: too many attibutes, table " + str(token.tableName) + " has " + str(len(table_attr_names)) 
-                        return False
-                elif (len(table_attr_names) > len(insert_attr_values)):
-                        print "\nError: too few attibutes, table " + str(token.tableName) + " has " + str(len(table_attr_names))
-                        return False
-                else:
-                        for index,tip in enumerate(insert_attr_types):
-                                if insert_attr_types[index] != int(table_attr_types[index]):
-                                        type_name = get_type_name(int(table_attr_types[index]))
-                                        print "\nError: type error for attribute '" + insert_attr_names[index] + "':"
-                                        akdbError(expr,insert_attr_values[index])
-                                        print "Expected: " + type_name
-                                        return False
-                if(ak47.insert_data_test(table_name,insert_attr_names,insert_attr_values,insert_attr_types) == ak47.EXIT_SUCCESS):
-                        return True
-                else:
-                        return False
-                return False
+                    if (len(insert_columns) == len(insert_attr_values)):
+                            for index,tip in enumerate(insert_attr_types):
+                                    if int(insert_attr_types[index]) != int(table_attr_types[index]):
+                                            type_name = get_type_name(int(table_attr_types[index]))
+                                            print "\nError: type error for attribute '" + insert_attr_names[index] + "':"
+                                            akdbError(expr,insert_attr_values[index])
+                                            print "Expected: " + type_name
+                                            return False
+                    else:
+                            print "\nError: attribute names number not matching attribute values number supplied for table '" + table_name + "':"
+                            akdbError(expr,insert_columns[0])
+                            return False
+            # values for insert
+            elif (len(table_attr_names) < len(insert_attr_values)):
+                    print "\nError: too many attibutes, table " + str(token.tableName) + " has " + str(len(table_attr_names)) 
+                    return False
+            elif (len(table_attr_names) > len(insert_attr_values)):
+                    print "\nError: too few attibutes, table " + str(token.tableName) + " has " + str(len(table_attr_names))
+                    return False
+            else:
+                    for index,tip in enumerate(insert_attr_types):
+                            if insert_attr_types[index] != int(table_attr_types[index]):
+                                    type_name = get_type_name(int(table_attr_types[index]))
+                                    print "\nError: type error for attribute '" + insert_attr_names[index] + "':"
+                                    akdbError(expr,insert_attr_values[index])
+                                    print "Expected: " + type_name
+                                    return False
+            if(ak47.insert_data_test(table_name,insert_attr_names,insert_attr_values,insert_attr_types) == ak47.EXIT_SUCCESS):
+                    return True
+            else:
+                    return False
+            return False
