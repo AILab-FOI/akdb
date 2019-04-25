fa900875136eb39c5eb5192304d24d57ef864bda
gugahaker <gugahaker@protonmail.com>
2019-04-25 21:02:34 +0200

    Server handles multiple connections and executes commands until connection is closed

--- akdb/src/srv/myclient.py
+++ akdb/src/srv/myclient.py
@@ -5,16 +5,19 @@ import paramiko
 HOST = "localhost"
 PORT = 1998
 
+username = "testingUser" # raw_input("Username: ")
+password = "testingPass" # raw_imput("Password: ")
+
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh_client.connect(HOST, port=PORT, username="testingUser", password="testingPass")
+ssh_client.connect(HOST, port=PORT, username=username, password=password)
 session = ssh_client.get_transport().open_session()
 
-print session.recv(1024)
-cmd = raw_input("akdb> ")
-session.send(cmd)
-print session.recv(1024)
+while True:
+    cmd = raw_input("akdb> ")
+    session.send(cmd)
+    print session.recv(1024)
 
 session.close()
 ssh_client.close()
--- akdb/src/srv/myserver.py
+++ akdb/src/srv/myserver.py
@@ -9,6 +9,14 @@ sys.path.append("../swig/")
 import kalashnikovDB as ak47
 import sql_executor as sqle
 
+
+def Threaded(fn):
+    def wrapper(*args, **kwargs):
+        t = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        t.start()
+    return wrapper
+
+
 class ParamikoServer(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
@@ -17,44 +25,79 @@ class ParamikoServer(paramiko.ServerInterface):
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
     def check_auth_password(self, username, password):
-        usr = "testingUser" #TODO change to real user
-        pas = "testingPass" #TODO change to real pass
+        usr = "testingUser" #TODO get users and passwords from database
+        pas = "testingPass"
         if (username == usr) and (password == pas):
             return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
 
-def process_command(cmd):
+class Connection:
+    def __init__(self, conn):
+        rsa_key = paramiko.RSAKey.generate(2048)        
+        self.transport = paramiko.Transport(conn)
+        self.transport.add_server_key(rsa_key)
+        self.transport.start_server(server=ParamikoServer())
+        self.channel = self.transport.accept()
+
+    def __del__(self):
+        self.channel.close()
+        self.transport.close()
+
+    def send_data(self, data):
+        #TODO implement protocol
+        self.channel.send(self.pack_output(data))
+
+    def recv_data(self):
+        #TODO implement protocol
+        return self.unpack_input(self.channel.recv(1024))
+
+    def pack_output(self, out):
+        #TODO pack into json
+        return out
+
+    def unpack_input(self, inp):
+        #TODO unpack json
+        inp = inp.strip()
+        return inp
+
+class Server:
+
     executor = sqle.sql_executor()
-    return executor.execute(cmd)
 
-sqle.initialize()
+    def __init__(self, host="localhost", port=1998):
+        self.host = host;
+        self.port = port;
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.transport = None
+        self.channel = None
+        self.working = True
+
+    def __del__(self):
+        self.working = False
+        self.sock.close()
+
+    def start(self):
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(1)
+        
+        while self.working:
+            conn, addr = self.sock.accept()
+            ssh_conn = Connection(conn)
+            self.handle_connection(ssh_conn)
+
+    @Threaded
+    def handle_connection(self, conn):
+        while True:
+            cmd = conn.recv_data()
+            out = Server.process_command(cmd)
+            conn.send_data(out)   
+
+    @staticmethod
+    def process_command(cmd):
+        return Server.executor.execute(cmd)
 
 
-HOST = "localhost"
-PORT = 1998
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-sock.bind((HOST,PORT))
-sock.listen(1)
-
-conn, addr = sock.accept()
-rsa_key = paramiko.RSAKey.generate(2048)
-transport = paramiko.Transport(conn)
-transport.add_server_key(rsa_key)
-transport.start_server(server=ParamikoServer())
-channel = transport.accept()
-
-channel.send("Hello")
-cmd = channel.recv(1024).strip()
-print cmd
-out = process_command(cmd)
-print out
-channel.send(out)
-
-channel.close()
-transport.close()
-conn.close()
-sock.close()
+s = Server()
+s.start()
 
