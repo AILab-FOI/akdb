00127bbb720aec724a5cb35058c0c2ebefe36d4f
gugahaker <gugahaker@protonmail.com>
2019-04-26 12:09:32 +0200

    Overwritten old server and client apps

--- akdb/src/srv/akdb_cli.py
+++ akdb/src/srv/akdb_cli.py
@@ -1,156 +1,96 @@
-# instructions
-# you start the client with
-#	python akdb_cli.py
-# and start sending the commands
-# you have an in-app help that you can raise up with \?,
-# and you can easily disconnect and exit with \q.
-import cmd
-import socket
-import colors
+#!/usr/bin/env python2
 
-welcome_note =  '''Welcome to the AKDB client. Enter your commands in the command line.
+import sys
+import time
+import paramiko
 
-To see list of all commands, type '\?'
-To see list of entered comands, type 'history'.
-'''
-
-##List of commands that don't send request to server
-#You should add commands to this list when needed
-client_specific_commands = ["", "\?", "history"]
-
-##command list, used to print out the help
-#update this list when new commands are added
-command_list =  {'\q': 'Disconnect from the server and quit akdb client.',
-				 '\?': 'List all commands supported in akdb.',
-				 '\d <table_name>' : 'Prints out table details',
-				 '\p <table_name>' : 'Prints out table',
-				 '\\t <table_name>?': 'Check whether the given table exists in database or not.',
-				 'create sequence <sequence_name> <sql_expression>' : 'Creating sequence in AK_sequence table',
-				 'create table <table_name> <sql_expression>' : 'Create table',
-				 'create view <view_name> as <sql_expression' : 'Create view',
-				 'create index <index_name> <sql_expression>' : 'Create index',
-				 'create trigger <trigger_name> <sql_expression>' : 'Create trigger',
-				 'create user <username> with password <password' : 'Create user',
-				 'create function <function_name> <args> returns <sql_expression> <language>' : 'Create function',
-				 'drop <sql_expression>' : 'Drop sql object',
-				 'grant <privilege_name> <sql_expression>' : 'Grant privilege',
-				 'select <sql_expression>' : 'Select data from table',
-				 'update <sql_expression>' : 'Update table',
-				 'insert into <table_name> <sql_expression>' : 'Insert command',
-				 'alter table <table_name> <sql_expression>' : 'Alter table',
-				 'alter index <index_name> <command>' : 'Alter index',
-				 'alter sequence <sequence_name> <command>' : 'Alter sequence',
-				 'alter view <view_name> <command>' : 'Alter view',
-				 'begin <sql_expression> commit' : 'Transaction',
-				 'history': 'List all previously typed commands.'
-				}
-
-##instantiate the colours class
-bcolors = colors.bcolors()
-
-##define data
-ip = "localhost"
-port = 1998
-buffer_size = 1024
+if len(sys.argv) == 3:
+    username = sys.argv[1]
+    password = sys.argv[2]
+else:
+    username = raw_input("Username: ")
+    password = raw_input("Password: ")
 
 
-class AK_Console(cmd.Cmd):
-	cli = None
+class Client:
+    def __init__(self, host="localhost", port=1998):
+        self.sock = paramiko.SSHClient()
+        self.sock.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.session = None
+        self.host = host
+        self.port = port
+        self.working = True
 
-	##Initialization of the console. Sets up the prompt and
-	#a welcome note.
-	def __init__(self):
-		cmd.Cmd.__init__(self)
-		self.prompt = "akdb>"
-		self.intro = welcome_note
+    def __del__(self):
+        self.working = False
+        if self.session is not None:
+            self.session.close()
+            print "[+] Session closed succesfully"
+        self.sock.close()
+        print "[+] Socket closed successfully"
 
-	##Setter for the AK_client class
-	def set_cli(self, cli):
-		self.cli = cli
+    def start(self):
+        global username
+        global password
+        connected = False
+        
+        while not connected:
+            print "[*] Attempting to connect..."
+            print username,password
+            try:
+                self.sock.connect(
+                    hostname=self.host, 
+                    port=self.port, 
+                    username=username, 
+                    password=password
+                )
+                connected = True
+            except paramiko.ssh_exception.AuthenticationException:
+                username = raw_input("Username: ")
+                password = raw_input("Password: ")
+            except Exception, e:
+                # wait 2 seconds before attempting to connect again
+                time.sleep(2)
 
-	##When empty line is pressed, call default and ignore it
-	def emptyline(self):
-		self.default("")
+        self.session = self.sock.get_transport().open_session()
+        print "[+] Successfully conected to server"
 
-	##Called when history command is called
-	def do_history(self, args):
-		print self._history
+        while self.working:
+            try:
+                cmd = raw_input("akdb> ")
+                self.send_command(cmd)
+                out = self.recv_data()
+                print out
+            except KeyboardInterrupt:
+                self.working = False
 
-	##Called before loop is started defines history list and local and
-	#global variables that are currently not used
-	def preloop(self):
-		cmd.Cmd.preloop(self)
-		self._history = []
-		self._locals = {}
-		self._globals = {}
 
-	##Called before the command is executed
-	def precmd(self, line):
-		self._history += [line.strip()]
-		return line
+    def send_command(self, cmd):
+        #TODO implement protocol
+        if len(cmd) > 0:
+            try:
+                self.session.send(self.pack_data(cmd))
+            except Exception, e:
+                print "[-] Sending failed: %s" %e
 
-	##Called after the command is eqecuted
-	def postcmd(self, stop, line):
-		if line in client_specific_commands:
-			return
-		response = self.cli.AK_check_response()
-		if response == "\q":
-			self.cli.set_finished(True)
-			return -1
-		else:
-			print response
+    def recv_data(self):
+        #TODO implement protocol
+        out = self.session.recv(1024)
+        return self.unpack_data(out)
 
-	##Called on default when the command is inserted
-	def default(self, line):
-		if line == "":
-			return
-		elif line == "\?":
-			self.print_commands()
-			return
-		line = line.strip()
-		self.cli.AK_send_command(line)
+    # packs data into json
+    def pack_data(self, data):
+        #TODO pack data
+        return data
 
-	##Prints the commands when the \? is inserted
-	def print_commands(self):
-		print bcolors.YELLOW + "Commands:"
-		for k, v in command_list.iteritems():
-			print bcolors.OKBLUE +  k + " - " + bcolors.OKGREEN + v + bcolors.ENDC
-		print ""
-		return
+    # unpacks json data
+    def unpack_data(self, data):
+        #TODO unpack data
+        return data
 
-##client class
-class AK_client():
-	sock = None
-	finished = False
+                    
 
-	##Connects to the server. runs a loop that holds the commands
-	def AK_client_connect(self, ip, port, buffer_size):
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.connect((ip, port))
-		self.AK_check_response()
-		c =  AK_Console()
-		c.set_cli(self)
-		while self.finished != True:
-			c.cmdloop()
-		print "\nAKDB now exiting."
+c = Client()
+c.start()
 
-	##Sends a command to server
-	def AK_send_command(self, command):
-		self.sock.send(command.strip())
-
-	##Checks for a command from the server
-	def AK_check_response(self):
-		return self.sock.recv(buffer_size)
-	##Setter for finished vairable
-	def set_finished(self, val):
-		self.finished = val
-
-	##Getter for the finished variable
-	def get_finished(self):
-		return self.finished
-
-cli = AK_client()
-
-if __name__ == "__main__":
-	cli.AK_client_connect(ip, port, buffer_size)
-
+        
--- akdb/src/srv/server.py
+++ akdb/src/srv/server.py
@@ -1,69 +1,127 @@
-# instructions
-# you start the server with:
-#   python server.py
-# and it runs indefinitely
-# currently it needs to be turned off manually ctrl+c
+#!/usr/bin/env python2
 
-import sys
-sys.path.append('../swig/')
-import SocketServer
 import socket
-import time
+import paramiko
+import threading
+import sys
+
+sys.path.append("../swig/")
 import kalashnikovDB as ak47
 import sql_executor as sqle
-import colors
-from user_log import logging
 
-logging.info('Server successfully started')
-##instantiate the colours class
-bcolors = colors.bcolors()
 
-class AK_server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
+def Threaded(fn):
+    def wrapper(*args, **kwargs):
+        t = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        t.start()
+    return wrapper
 
-## request_handler_class
-# Handles requests
-class AK_server_request_handler(SocketServer.BaseRequestHandler):
-    executor = sqle.sql_executor()
 
-    ## setup method
-    # Called when client connects, before handle
-    def setup(self):
-        print self.client_address, "connected"
-        self.request.send("Connection successful")
+class ParamikoServer(paramiko.ServerInterface):
+    def __init__(self):
+        self.event = threading.Event()
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+    def check_auth_password(self, username, password):
+        usr = "testingUser" #TODO get users and passwords from database
+        pas = "testingPass"
+        if (username == usr) and (password == pas):
+            return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
 
-    ## handle method
-    # Handles requests by calling the processCommand method.
-    def handle(self):
+class Connection:
+    def __init__(self, conn, addr):
+        self.addr = addr
+        self.transport = paramiko.Transport(conn)
+        self.transport.add_server_key(Server.rsa_key)
+        self.transport.start_server(server=ParamikoServer())
+        self.channel = self.transport.accept(timeout=1)
+
+    def __del__(self):
+        if self.channel is not None:
+            self.channel.close()
+        if self.transport is not None:
+            self.transport.close()
+
+    def send_data(self, data):
+        #TODO implement protocol
         try:
-            while True:
-                response = self.processCommand(self.request.recv(1024).strip())
-                if response == "\q":
-                    self.request.send(response)
-                    return
-                else:
-                    self.request.send(response)
-        except socket.error, e:
-            print bcolors.FAIL + "An error occured. Probably the client disconnected violently." + bcolors.ENDC
+            self.channel.send(self.pack_output(data))
+        except Exception, e:
+            print "[-] Failed sending data to client: %s" %e
 
-    ## finish method
-    #Called when client disconnects, after handle.
-    def finish(self):
-        print self.client_address, "disconnected"
-    ## process command method
-    # Used to process a command given from a client.
-    # Here is where you should call your application logic code.
-    def processCommand(self, command):
-	logging.info(command)
-        if command=="\q":
-            return command
-        else:
-            return self.executor.execute(command)
+    def recv_data(self):
+        #TODO implement protocol
+        return self.unpack_input(self.channel.recv(1024))
 
-if __name__ == "__main__":
-    ip = "localhost"
-    port = 1998
-    sqle.initialize()
-    print "\nServer is up and running!\nPort number: 1998\n"
-    server = AK_server((ip, port), AK_server_request_handler)
-    server.serve_forever()
+    def pack_output(self, out):
+        #TODO pack into json
+        return out
+
+    def unpack_input(self, inp):
+        #TODO unpack json
+        inp = inp.strip()
+        return inp
+
+    def is_alive(self):
+        if self.channel is not None and self.transport.is_active():
+            return True
+        return False
+
+class Server:
+
+    executor = sqle.sql_executor()
+    rsa_key = paramiko.RSAKey.generate(2048)        
+
+    def __init__(self, host="localhost", port=1998):
+        self.host = host;
+        self.port = port;
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.transport = None
+        self.channel = None
+        self.working = True
+
+    def __del__(self):
+        self.working = False
+        self.sock.close()
+
+    def start(self):
+        try:
+            self.sock.bind((self.host, self.port))
+        except Exception, e:
+            print "[-] Failed to start server: %s" %e
+            self.__del__()
+
+        self.sock.listen(1)
+        
+        while self.working:
+            conn, addr = self.sock.accept()
+            print "[*] Incoming connection from %s" %addr[0]
+            ssh_conn = Connection(conn, addr)
+            self.handle_connection(ssh_conn)
+
+    @Threaded
+    def handle_connection(self, conn):
+        while conn.is_alive():
+            cmd = conn.recv_data()
+            out = Server.process_command(cmd)
+            conn.send_data(out)   
+        print "[*] Client from %s disconnected from server" % conn.addr[0]
+        del conn
+
+    @staticmethod
+    def process_command(cmd):
+        try:
+            out = Server.executor.execute(cmd)
+        except Exception, e:
+            err = "[-] Failed command execution: %s" %e
+            out = err
+            print err
+        return out
+
+s = Server()
+s.start()
+
