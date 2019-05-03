57ebbf2fe29909215073ffe6f98ef600811d7784
Markus Schatten <markus.schatten@foi.hr>
2019-05-03 21:53:13 +0200

    Forgot original insert files by fzmuk.

---
+++ akdb/src/sql/insert.h
@@ -0,0 +1,40 @@
+/**
+ * @file insert.h Header file SQL insert command.
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+
+#ifndef SQL_INSERT
+#define SQL_INSERT
+
+#include "../auxi/mempro.h"
+#include "../auxi/test.h"
+#include "../file/fileio.h"
+
+#include "../auxi/constants.h"
+#include "../file/table.h"
+#include "drop.h"
+
+AK_header *AK_get_insert_header(int *size, char *tblName, struct list_node *columns);
+
+int AK_insert(char* tableName, struct list_node *columns, struct list_node *values);
+
+#ifndef SWIG
+TestResult AK_insert_test();
+#endif
+
+#endif
