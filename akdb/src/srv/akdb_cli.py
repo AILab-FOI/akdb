2b6e82db1ebb203140b7368909f3f7f7e374cbb9
DarioSiroki <dario.siroki@gmail.com>
2022-03-13 14:56:00 +0100

    Remove TODO
    This was done in 643c5ac6d252e5163c8733c3d6ce2465292575e5

--- akdb/src/srv/akdb_cli.py
+++ akdb/src/srv/akdb_cli.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
+import kalashnikovDB as ak47
 import sys
 from client import Client
 
 sys.path.append("../swig/")
-import kalashnikovDB as ak47
 
-#TODO refactor code ( split logic to multiple files)
 if len(sys.argv) == 3:
     username = sys.argv[1]
     password = sys.argv[2]
