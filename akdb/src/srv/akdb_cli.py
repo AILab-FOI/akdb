AILab-FOI/akdb

commit 2b6e82db1ebb203140b7368909f3f7f7e374cbb9

Remove TODO
This was done in 643c5ac6d252e5163c8733c3d6ce2465292575e5

akdb/src/srv/akdb_cli.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
+import kalashnikovDB as ak47
 import sys
 from client import Client
 
 sys.path.append("../swig/")
-import kalashnikovDB as ak47
 
-#TODO refactor code ( split logic to multiple files)
 if len(sys.argv) == 3:
     username = sys.argv[1]
     password = sys.argv[2]

commit 754b709c4f53ee2acd48a4d32413209227667818

Fix import error
Caused by 2b6e82db1ebb203140b7368909f3f7f7e374cbb9. My PEP linter automatically did this. kalashnikovDB would not get imported because sys.path.append was after the import.

akdb/src/srv/akdb_cli.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
-import kalashnikovDB as ak47
 import sys
 from client import Client
 
 sys.path.append("../swig/")
+import kalashnikovDB as ak47
 
 if len(sys.argv) == 3:
     username = sys.argv[1]